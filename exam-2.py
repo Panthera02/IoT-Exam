a77a8d048afd39bc3698003193ae1ed1cc20104d
Panthera02 <119818435+Panthera02@users.noreply.github.com>
2023-12-20 11:39:00 +0100

    Add files via upload

--- exam-2.py
+++ exam-2.py
@@ -7,14 +7,16 @@ sense = SenseHat()
 letter = '@'
 
 def create():
-    letter += 1
-    return """{"ID": %c, "city": "Madrid", "humidity": %f}"""%(letter, sense.get_humidity())
+    global letter
+    letter = chr(ord(letter)+ 1)
+    return """{"ID": "%c", "city": "Madrid", "humidity": %f}"""%(letter, sense.get_humidity())
 
 client = mqtt.Client()
 client.connect("broker.emqx.io", 1883, 60)
 client.loop_start()
 
 while True:
+    #print(sense.get_humidity())
     if sense.get_humidity() > 65 :client.publish("/ETSIDI/Hum", create())
     sleep(3)
 
